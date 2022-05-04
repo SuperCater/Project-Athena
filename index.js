554c7fb32c989e0b6b2136c6456f9b9c0f56a276
SuperCater <supercater500@gmail.com>
2022-05-04 17:47:52 -0400

    Tried to fix ban command
    Lets see if it works

--- index.js
+++ index.js
@@ -1,50 +1,35 @@
-const Discord = require('discord.js')
+const Discord = require("discord.js")
+
 require("dotenv").config()
 
 const client = new Discord.Client({
-    intents: [
-        "GUILDS",
-        "GUILD_MESSAGES",
-        "GUILD_MEMBERS",
-        "GUILD_BANS"
-    ]
+    intents: ["GUILDS"]
 })
 
 let bot = {
-    client,
-    prefix: "c.",
-    owners: ["439514395534688257"]
+    client
 }
-
-client.commands = new Discord.Collection()
-client.events = new Discord.Collection()
-
-client.loadEvents = (bot, reload) => require("./handlers/events")(bot, reload)
-client.loadCommands = (bot, reload) => require("./handlers/commands")(bot, reload)
-
-client.loadEvents(bot, false)
-client.loadCommands(bot, false)
-module.exports = bot
 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}`)
 })
 
-client.slashcommands = new Discord.Collection()
+client.slashcommands = new Discord.Collection() 
 
 client.loadSlashCommands = (bot, reload) => require("./handlers/slashcommands")(bot, reload)
 client.loadSlashCommands(bot, false)
 
 client.on("interactionCreate", (interaction) => {
-    if (!interaction.isCommand()) return
-    if (!interaction.inGuild()) return interaction.reply("This command can only be used in a server!")
+    if (!interaction.isCommand()) return 
+    if (!interaction.inGuild()) return interaction.reply("This command can only be used in a server")
 
     const slashcmd = client.slashcommands.get(interaction.commandName)
 
     if (!slashcmd) return interaction.reply("Invalid slash command")
 
-    if (slashcmd.perms && !interaction.mamber.permissions.has(slashcmd.perm))
-    return interaction.reply("You do not have permission for this command!")
+    if (slashcmd.perm && !interaction.member.permissions.has(slashcmd.perm))
+        return interaction.reply("You do not have permission for this command")
+
     slashcmd.run(client, interaction)
 })
 
