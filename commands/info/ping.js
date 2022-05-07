d5eaf66d634231d1292350f054cf7bef0f55d618
SuperCater <supercater500@gmail.com>
2022-05-07 14:53:04 -0400

    Rewrite progress
    Have remade index.js and changed how commands are deployed. No commands active yet.

--- commands/info/ping.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: "ping",
-    category: "info",
-    permissions: [],
-    devOnly: false,
-    run: async ({bot, message, args}) => {
-        message.reply("Pong")
-    }
-}
--- index.js
+++ index.js
@@ -1,36 +1,14 @@
-const Discord = require("discord.js")
-
+// Require the necessary discord.js classes
+const { Client, Intents } = require('discord.js');
 require("dotenv").config()
 
-const client = new Discord.Client({
-    intents: ["GUILDS"]
-})
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-let bot = {
-    client
-}
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+	console.log(`Logged in as ${client.user.tag}!`);
+});
 
-client.on("ready", () => {
-    console.log(`Logged in as ${client.user.tag}`)
-})
-
-client.slashcommands = new Discord.Collection() 
-
-client.loadSlashCommands = (bot, reload) => require("./handlers/slashcommands")(bot, reload)
-client.loadSlashCommands(bot, false)
-
-client.on("interactionCreate", (interaction) => {
-    if (!interaction.isCommand()) return 
-    if (!interaction.inGuild()) return interaction.reply("This command can only be used in a server")
-
-    const slashcmd = client.slashcommands.get(interaction.commandName)
-
-    if (!slashcmd) return interaction.reply("Invalid slash command")
-
-    if (slashcmd.perm && !interaction.member.permissions.has(slashcmd.perm))
-        return interaction.reply("You do not have permission for this command")
-
-    slashcmd.run(client, interaction)
-})
-
-client.login(process.env.TOKEN)
+// Login to Discord with your client's token
+client.login(process.env.TOKEN);
