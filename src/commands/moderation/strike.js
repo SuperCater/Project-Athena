ffbfa6bd4fe4faf7615f77fa15d583b18615137a
SuperCater <supercater500@gmail.com>
2022-11-23 16:41:37 -0500

    Made strike use sub-commands

--- src/commands/moderation/strike.js
+++ src/commands/moderation/strike.js
@@ -5,13 +5,24 @@ module.exports = {
         .setName('strike')
         .setDescription('Strike a user.')
         .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
-        .addUserOption(option => option.setName('user').setDescription('The user to strike.').setRequired(true)),
+        .addSubcommand(subcommand => subcommand
+            .setName('add')
+            .setDescription('Add a strike to a user.')
+            .addUserOption(option => option.setName('user').setDescription('The user to strike.').setRequired(true)))
+        .addSubcommand(subcommand => subcommand
+            .setName('remove')
+            .setDescription('Remove a strike from a user.')
+            .addUserOption(option => option.setName('user').setDescription('The user to remove a strike from.').setRequired(true))),
+
+
     async execute(interaction, client) {
             const user = interaction.options.getUser('user');
             const member = interaction.guild.members.cache.get(user.id);
+            const subcommand = interaction.options.getSubcommand();
             const strike1 = interaction.guild.roles.cache.find(role => role.name === "Strike 1");
             const strike2 = interaction.guild.roles.cache.find(role => role.name === "Strike 2");
 
+            if (subcommand === 'add') {
             if (member.roles.cache.has(strike1.id)) {
                 if (member.roles.cache.has(strike2.id)) {
                     await interaction.reply(`This user already has 2 strikes.`);
@@ -23,6 +34,15 @@ module.exports = {
                 member.roles.add(strike1);
                 await interaction.reply(`Striked ${user.username}.`);
             }
+        } else if (subcommand === 'remove') {
+            if (member.roles.cache.has(strike2.id)) {
+                member.roles.remove(strike2);
+                await interaction.reply(`Removed 2nd strike from ${user.username}.`);
+            } else if (member.roles.cache.has(strike1.id)) {
+                member.roles.remove(strike1);
+                await interaction.reply(`Removed first strike from ${user.username}.`);
+            }
+        }
     },
     type: "GSL",
 }
